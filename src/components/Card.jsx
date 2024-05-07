880851fddd68a79b0b0c3589d11fd5443a9658a5
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-05-07 15:17:08 -0300

    done ___PRODUTOS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
+import "./Style.css";
+import { useState } from "react";
 /* eslint-disable react/prop-types */
 export const Card = (props) => {
-  return(
-      <div>
-          <h1>{props.name}</h1>
-          <h2>{props.desc}</h2>
-          <p>{props.value}</p>
-          <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+  return (
+    <div className="Ctn_background">
+      <h1>{props.name}</h1>
+      <div className="Ctn_Balls">
+        <h2>{props.desc}</h2>
+        <div className={props.status ? "lil_Ball" : "lil_Ball_On"}></div>
       </div>
+      <p>{props.value}</p>
+      <img src={props.image} alt={props.name} width={150} height={"auto"} />
+    </div>
   )
 }
