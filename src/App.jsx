69acfed64d530bdff8347569a56ebc226a93e5fd
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-05-07 15:57:26 -0300

    finishied

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ function App() {
 
   useEffect(() => {
     api.get(`/character/?page=${page}`).then((response) => {
-      if(!response.data.results){
+      if (!response.data.results) {
         console.log("Vazio")
       }
       setData(response.data.results)
     }).catch((error) => {
-      if(error.response.status === 404){
+      if (error.response.status === 404) {
         console.log("Esta pagina nao contem este personagem")
       }
       console.error(error)
@@ -26,52 +26,64 @@ function App() {
 
   return (
     <>
-    <div className={style.wrapBtns}>
-      <button onClick={() => setShow("prod")}>Produtos</button>
-      <button onClick={() => setShow("api")}>API</button>
-      <button onClick={() => setShow("map")}>Mapa</button>
-    </div>
-    <div  className={style.wrapPage}>
-      <h1>Exercícios de manutenção</h1>
-     {show === "prod" &&
-        <>
-          <h2>Showroom de produtos</h2>
-            <div>
-            {produtos.map((item) => {
-              return(
-                <Card name={item.name} desc={item.desc} value={item.value} image={item.image} key={item.id} status={item.status}/>
-              )
-             })}
-            </div>
-        </>
-      }
-     {show === "api" &&
-        <>
-          <h2>Rick and Morty API</h2>
-            <div>
-               <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
-            </div>
-            <div>
-            {data.map((item) => { 
-             return(
-              <div key={item.id}>
-                <Card name={item.name} desc={item.species} value={item.gender} image={item.image} />
-                {/* <button onClick={() => {}}>Info</button> */}
+      <div className={style.wrapBtns}>
+        <button onClick={() => setShow("prod")}>Produtos</button>
+        <button onClick={() => setShow("api")}>API</button>
+        <button onClick={() => setShow("map")}>Mapa</button>
+      </div>
+      <div className={style.wrapPage}>
+        <h1>Exercícios de manutenção</h1>
+        {show === "prod" &&
+          <>
+            <div className={style.subtitle}>
+              <h2>Showroom de produtos</h2>
+              <div>
+                <div className={style.display}>
+                  <div className={style.lil_Ball}></div>
+                  <h1>mostruario</h1>
+                </div>
+                <div className={style.display}>
+                  <div className={style.lil_Ball_On}></div>
+                  <h1>venda</h1>
+                </div>
               </div>
-              )
-           })}
             </div>
-       </>
-      }
-     {show === "map" &&
-        <>
-      <h2>Mapa</h2>
-          <div>
+            <div className={style.Ctn_Card}>
+              {produtos.map((item) => {
+                return (
+                  <Card name={item.name} desc={item.desc} value={item.value} image={item.image} key={item.id} status={item.status} category={item.categ} />
+                )
+              })}
+            </div>
+          </>
+        }
+        {show === "api" &&
+          <>
+            <h2>Rick and Morty API</h2>
+            <div>
+              <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)} />
+            </div>
+            <div>
+              {data.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <Card name={item.name} desc={item.species} value={item.gender} image={item.image} />
+                    {/* <button onClick={() => {}}>Info</button> */}
+                  </div>
+                )
+              })}
+            </div>
+          </>
+        }
+        {show === "map" &&
+          <>
+            <h2>Mapa</h2>
+            <div>
               mapa aqui
-          </div>
-         </>
-      }
-    </div>
+            </div>
+          </>
+        }
+      </div>
     </>
   )
 }
